9721e5eb9842b11db5cd119a787b5e51077b359e
kiptook7070 <kiptook2020@gmail.com>
2024-01-16 12:50:22 +0300

    api gateway ininitial commit

---
+++ apigateway/src/main/java/com/techmonks/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.techmonks.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
